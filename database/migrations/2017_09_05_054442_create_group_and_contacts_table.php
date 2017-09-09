14e8e30a9605bf81bb1ade11c228ed69d6c0645e
MH Raihan <me@mhraihan.com>
2017-09-10 03:06:52 +0600

    migrate:fresh --seed

--- database/migrations/2017_09_05_054442_create_group_and_contacts_table.php
+++ database/migrations/2017_09_05_054442_create_group_and_contacts_table.php
@@ -23,7 +23,7 @@ class CreateGroupAndContactsTable extends Migration
         Schema::create('contacts', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('group_id')->unsigned()->default(0);
-            $table->foreign('group_id')->refrences('id')->on('groups')->onDelete('cascade');
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
             $table->string('name');
             $table->string('company');
             $table->string('email');
